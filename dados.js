d2b28d36c3fbc5c308db1dc68f671592cf7dd429
ProfRafaLeandro <rafael.furlan.leandro@gmail.com>
2024-09-05 16:58:03 -0300

    terceira aula

---
+++ dados.js
@@ -0,0 +1,8 @@
+let dados = [
+    "Medicina", "Engenharia de Software", "Engenharia Elétrica", "Engenharia Ambiental",
+    "Engenharia Mecânica", "Engenharia Civil", "Engenharia Eletrônica", "Engenharia Mecatrônica",
+    "Engenharia da Computação", "Engenharia Aeroespacial", "Engenharia Naval", "Engenharia Aeronáutica",
+    "Engenharia de Alimentos", "Engenharia de Produção", "Direito", "Sociologia", "Filosofia", "História",
+    "Geografia", "Antropologia", "Letras", "Biblioteconomia", "Fisioterapia", "Enfermagem", "Terapia Ocupacional",
+    "Fonoaudiologia"
+]
